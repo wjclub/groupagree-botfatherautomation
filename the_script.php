ef2c0766f393c6292e195ec637c3ed8614d913be
Jan Braun <browny@wjclub.tk>
2018-06-26 08:33:42 +0200

    Finished the bot
    Ay hope, because you know, php

--- the_script.php
+++ the_script.php
@@ -1,29 +1,30 @@
 $apikey = $_GET['apikey'];
 if (hash('sha512',$apikey) == '') {
-		define('API_KEY',$apikey);
+	define('API_KEY',$apikey);
 } else {
-		exit('We\'re done here Mr. '.$_SERVER['REMOTE_ADDR'].' '.$_SERVER['HTTP_X_FORWARDED_FOR']);
+	exit('We\'re done here Mr. '.$_SERVER['REMOTE_ADDR'].' '.$_SERVER['HTTP_X_FORWARDED_FOR']);
 }
- 
-
-
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
+$result = preg_replace("/[^a-zA-Z0-9]+/", "", $update['message']['text']);
+$botusername = strtolower($result);
+sendMessage($update['message']['chat']['id'],"/newbot");
+sendMessage($update['message']['chat']['id'],$update['message']['text']);
+sendMessage($update['message']['chat']['id'],'@'.$botusername);
+sendMessage($update['message']['chat']['id'],"/setinline");
+sendMessage($update['message']['chat']['id'],'@'.$botusername);
+sendMessage($update['message']['chat']['id'],"Search polls...");
+sendMessage($update['message']['chat']['id'],"/setinlinefeedback");
+sendMessage($update['message']['chat']['id'],'@'.$botusername);
+sendMessage($update['message']['chat']['id'],"Enabled");
+sendMessage($update['message']['chat']['id'],"/setjoingroups");
+sendMessage($update['message']['chat']['id'],'@'.$botusername);
+sendMessage($update['message']['chat']['id'],"Disable");
+sendMessage($update['message']['chat']['id'],"/setcommands");
+sendMessage($update['message']['chat']['id'],'@'.$botusername);
+sendMessage($update['message']['chat']['id'],"start - 📝 Create a new poll\nlist - 📋 List all polls\ncancel - 🚫 Cancel the current operation\nhelp - ℹ️ Get help\nlang - 🗣Change language");
 
- 
-if ($exp_msg_text[0] == "/start") {
-	sendMessage($update['message']['chat']['id'],"Hello, \nI can extract the bot token from the ugly message the @botfather sends you...\n\n<b>NOTE: WE DO NOT STORE ANYTHING YOU SEND US (INCLUDING THE TOKEN)!</b>\n\n<i>Source code: </i>https://github.com/wjclub/telegram-bot-tokenextract"); 
-} else {
-	$res = getToken($update['message']['text']);
-	if ($res['ok'] == true) {
-		sendMessage($update['message']['chat']['id'],$res['bot_info']);
-		sendMessage($update['message']['chat']['id'],'<code>'.$res['token'].'</code>');
-	} else {
-		sendMessage($update['message']['chat']['id'],'"<code>'.$res['token'].'</code>" is not a valid bot token...'."\nCorrect your input or contact @wjclub about it");
-	}
-}
- 
-function sendMessage($chat_id,$reply){
+function sendMessage($chat_id,$reply) {
 	$reply_content = [
 	'method' => "sendMessage",
 	'chat_id' => $chat_id,
@@ -31,7 +32,6 @@ function sendMessage($chat_id,$reply){
 	'text' => $reply,
 	];
 	$reply_json = json_encode($reply_content);
-//async request
 	$url = 'https://api.telegram.org/bot'.API_KEY.'/';
 	$cmd = "curl -s -X POST -H 'Content-Type:application/json'";
 	$cmd.= " -d '" . $reply_json . "' '" . $url . "'";
